7d456075bc253f8576ea7118ab37825e42631200
laditeo <t3oc@tuta.io>
2026-07-13 19:54:32 +0700

    Disable MOUSEKEY: fixes consumer/media reports being misrouted as mouse

--- keyboards/sofle/rev1/keymaps/webconfig/rules.mk
+++ keyboards/sofle/rev1/keymaps/webconfig/rules.mk
@@ -2,7 +2,11 @@
 # actions over USB (WebHID).
 RAW_ENABLE = yes
 
-# Required so the encoder can emit consumer/media keys (volume, mute, play...)
-# and mouse actions selected in the web app.
+# Required so the encoder can emit consumer/media keys (volume, mute, play...).
 EXTRAKEY_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+
+# NOTE: MOUSEKEY is intentionally disabled. On the atmega32u4 the keyboard,
+# mouse, consumer and Raw HID reports share a single USB endpoint, and enabling
+# the mouse HID collection was causing consumer (volume/media) reports to be
+# misrouted as mouse events in some applications.
+MOUSEKEY_ENABLE = no
